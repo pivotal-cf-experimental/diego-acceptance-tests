14952984a2b70d2eaec5f7327d4f0cfb5084957d
Matthew Sykes <matthew.sykes@gmail.com>
2014-10-23 14:19:19 -0600

    set CF_DIEGO_STAGE_BETA explicitly [#80165358]
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>

--- diego/instance_reporting_test.go
+++ diego/instance_reporting_test.go
@@ -17,6 +17,7 @@ var _ = Describe("Getting instance information", func() {
 		appName = generator.RandomName()
 
 		Eventually(cf.Cf("push", appName, "-p", assets.NewAssets().Standalone, "--no-start", "-b", DIEGO_NULL_BUILDPACK), CF_PUSH_TIMEOUT).Should(Exit(0))
+		Eventually(cf.Cf("set-env", appName, "CF_DIEGO_STAGE_BETA", "true")).Should(Exit(0))
 		Eventually(cf.Cf("set-env", appName, "CF_DIEGO_RUN_BETA", "true")).Should(Exit(0))
 
 		Eventually(cf.Cf("scale", appName, "-i", "3")).Should(Exit(0))
--- diego/lifecycle_test.go
+++ diego/lifecycle_test.go
@@ -19,8 +19,9 @@ var _ = Describe("Application Lifecycle", func() {
 		It("should staged and run on diego without problem", func() {
 			appName = generator.RandomName()
 			Eventually(cf.Cf("push", appName, "-p", assets.NewAssets().Dora, "--no-start"), CF_PUSH_TIMEOUT).Should(Exit(0))
-			Eventually(cf.Cf("set-env", appName, "CF_DIEGO_RUN_BETA", "true")).Should(Exit(0)) // CF_DIEGO_RUN_BETA also implies CF_DIEGO_STAGE_BETA in CC
-			Eventually(cf.Cf("start", appName), 2*CF_PUSH_TIMEOUT).Should(Exit(0))             // Double timeout to allow for all buildpacks.
+			Eventually(cf.Cf("set-env", appName, "CF_DIEGO_STAGE_BETA", "true")).Should(Exit(0))
+			Eventually(cf.Cf("set-env", appName, "CF_DIEGO_RUN_BETA", "true")).Should(Exit(0))
+			Eventually(cf.Cf("start", appName), 2*CF_PUSH_TIMEOUT).Should(Exit(0)) // Double timeout to allow for all buildpacks.
 		})
 
 		AfterEach(func() {
@@ -74,7 +75,8 @@ var _ = Describe("Application Lifecycle", func() {
 
 		Describe("An app both staged and run with Diego", func() {
 			BeforeEach(func() {
-				Eventually(cf.Cf("set-env", appName, "CF_DIEGO_RUN_BETA", "true")).Should(Exit(0)) // CF_DIEGO_RUN_BETA also implies CF_DIEGO_STAGE_BETA in CC
+				Eventually(cf.Cf("set-env", appName, "CF_DIEGO_STAGE_BETA", "true")).Should(Exit(0))
+				Eventually(cf.Cf("set-env", appName, "CF_DIEGO_RUN_BETA", "true")).Should(Exit(0))
 				Eventually(cf.Cf("start", appName), CF_PUSH_TIMEOUT).Should(Exit(0))
 			})
 
@@ -118,7 +120,8 @@ var _ = Describe("Application Lifecycle", func() {
 				Eventually(cf.Cf("stop", appName)).Should(Exit(0))
 				Eventually(helpers.CurlingAppRoot(appName)).Should(ContainSubstring("404"))
 
-				Eventually(cf.Cf("set-env", appName, "CF_DIEGO_RUN_BETA", "true")).Should(Exit(0)) // CF_DIEGO_RUN_BETA also implies CF_DIEGO_STAGE_BETA in CC
+				Eventually(cf.Cf("set-env", appName, "CF_DIEGO_STAGE_BETA", "true")).Should(Exit(0))
+				Eventually(cf.Cf("set-env", appName, "CF_DIEGO_RUN_BETA", "true")).Should(Exit(0))
 				Eventually(cf.Cf("push", appName, "-p", assets.NewAssets().Standalone, "-b", DIEGO_NULL_BUILDPACK), CF_PUSH_TIMEOUT).Should(Exit(0))
 			})
 
