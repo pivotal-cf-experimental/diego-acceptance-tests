8d5cea434a2c74bd23fa34a7464f19b377ad2968
Matthew Sykes <matthew.sykes@gmail.com>
2014-10-24 09:46:24 -0400

    Cleanup unused assets

--- helpers/assets/assets.go
+++ helpers/assets/assets.go
@@ -1,35 +1,15 @@
 package assets
 
 type Assets struct {
-	Dora                     string
-	HelloWorld               string
-	Node                     string
-	NodeWithProcfile         string
-	Java                     string
-	Golang                   string
-	Standalone               string
-	Python                   string
-	LoggregatorLoadGenerator string
-	ServiceBroker            string
-	Php                      string
-	SecurityGroupBuildpack   string
-	Fuse                     string
+	Dora       string
+	HelloWorld string
+	Standalone string
 }
 
 func NewAssets() Assets {
 	return Assets{
-		Dora:             "../assets/dora",
-		HelloWorld:       "../assets/hello-world",
-		Node:             "../assets/node",
-		NodeWithProcfile: "../assets/node-with-procfile",
-		Java:             "../assets/java",
-		Golang:           "../assets/golang",
-		Standalone:       "../assets/standalone",
-		Python:           "../assets/python",
-		LoggregatorLoadGenerator: "../assets/loggregator-load-generator",
-		ServiceBroker:            "../assets/service_broker",
-		Php:                      "../assets/php",
-		SecurityGroupBuildpack: "../assets/security_group_buildpack.zip",
-		Fuse: "../assets/fuse-mount",
+		Dora:       "../assets/dora",
+		HelloWorld: "../assets/hello-world",
+		Standalone: "../assets/standalone",
 	}
 }
