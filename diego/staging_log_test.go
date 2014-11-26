b9ada1a32ba633561379c8ad9a3484e4220582eb
Alex Suraci <asuraci@pivotal.io>
2014-11-26 11:47:02 -0800

    fix casing
    Signed-off-by: Michael Fraenkel <fraenkel@us.ibm.com>

--- diego/staging_log_test.go
+++ diego/staging_log_test.go
@@ -29,7 +29,7 @@ var _ = Describe("An application being staged with Diego", func() {
 		start := cf.Cf("start", appName)
 
 		Eventually(start, CF_PUSH_TIMEOUT).Should(Exit(0))
-		Expect(start).Should(Say("Downloaded App Package"))
+		Expect(start).Should(Say("Downloaded app package"))
 		Expect(start).Should(Say(`Staging\.\.\.`))
 	})
 })
