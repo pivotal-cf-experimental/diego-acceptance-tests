1200727f6a424912d0d4dbf2a1850195164e3484
Michael Fraenkel <fraenkel@us.ibm.com>
2015-01-26 14:15:51 -0500

    Add test to flow Insufficient resource to CC
    [#86856228]
    
    Signed-off-by: Amit Gupta <agupta@pivotal.io>

--- diego/staging_failure_test.go
+++ diego/staging_failure_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
 	"github.com/cloudfoundry-incubator/diego-acceptance-tests/helpers/assets"
 	"github.com/onsi/gomega/gbytes"
 )
@@ -14,28 +15,62 @@ import (
 var _ = Describe("When staging fails", func() {
 	var appName string
 
-	BeforeEach(func() {
-		appName = generator.RandomName()
+	Context("due to a custom buildpack that cannot be downloaded", func() {
+		BeforeEach(func() {
+			appName = generator.RandomName()
 
-		//Diego needs a custom buildpack until the ruby buildpack lands
-		Eventually(cf.Cf("push", appName, "-p", assets.NewAssets().Dora, "--no-start", "-b=http://example.com/so-not-a-thing/adlfijaskldjlkjaslbnalwieulfjkjsvas.zip"), CF_PUSH_TIMEOUT).Should(Exit(0))
-		Eventually(cf.Cf("set-env", appName, DIEGO_STAGE_BETA, "true")).Should(Exit(0))
+			//Diego needs a custom buildpack until the ruby buildpack lands
+			Eventually(cf.Cf("push", appName, "-p", assets.NewAssets().Dora, "--no-start", "-b=http://example.com/so-not-a-thing/adlfijaskldjlkjaslbnalwieulfjkjsvas.zip"), CF_PUSH_TIMEOUT).Should(Exit(0))
+			Eventually(cf.Cf("set-env", appName, DIEGO_STAGE_BETA, "true")).Should(Exit(0))
+		})
+
+		AfterEach(func() {
+			Eventually(cf.Cf("logs", appName, "--recent")).Should(Exit())
+			Eventually(cf.Cf("delete", appName, "-f")).Should(Exit(0))
+		})
+
+		It("informs the user in the CLI output and the logs", func() {
+			start := cf.Cf("start", appName)
+			Eventually(start, CF_PUSH_TIMEOUT).Should(Exit(1))
+			Ω(start.Out).Should(gbytes.Say("Staging error: cannot get instances since staging failed"))
+
+			Eventually(func() *Session {
+				logs := cf.Cf("logs", appName, "--recent")
+				Expect(logs.Wait()).To(Exit(0))
+				return logs
+			}).Should(gbytes.Say("Failed to download buildpack"))
+		})
 	})
 
-	AfterEach(func() {
-		Eventually(cf.Cf("logs", appName, "--recent")).Should(Exit())
-		Eventually(cf.Cf("delete", appName, "-f")).Should(Exit(0))
-	})
+	Context("due to insufficient resources", func() {
+		BeforeEach(func() {
+			appName = generator.RandomName()
 
-	It("informs the user in the CLI output and the logs", func() {
-		start := cf.Cf("start", appName)
-		Eventually(start, CF_PUSH_TIMEOUT).Should(Exit(1))
-		Ω(start.Out).Should(gbytes.Say("Staging error: cannot get instances since staging failed"))
+			context.SetRunawayQuota()
 
-		Eventually(func() *Session {
-			logs := cf.Cf("logs", appName, "--recent")
-			Expect(logs.Wait()).To(Exit(0))
-			return logs
-		}).Should(gbytes.Say("Failed to download buildpack"))
+			Eventually(cf.Cf("push", appName, "-p", assets.NewAssets().Dora, "--no-start", "-b=ruby_buildpack", "-m", helpers.RUNAWAY_QUOTA_MEM_LIMIT), CF_PUSH_TIMEOUT).Should(Exit(0))
+			Eventually(cf.Cf("set-env", appName, DIEGO_STAGE_BETA, "true")).Should(Exit(0))
+		})
+
+		AfterEach(func() {
+			Eventually(cf.Cf("logs", appName, "--recent")).Should(Exit())
+			Eventually(cf.Cf("delete", appName, "-f")).Should(Exit(0))
+		})
+
+		It("informs the user in the CLI output and the logs", func() {
+			start := cf.Cf("start", appName)
+			Eventually(start, CF_PUSH_TIMEOUT).Should(Exit(1))
+			Ω(start.Out).Should(gbytes.Say("message: Insufficient resources"))
+
+			Eventually(func() *Session {
+				logs := cf.Cf("logs", appName, "--recent")
+				Expect(logs.Wait()).To(Exit(0))
+				return logs
+			}).Should(gbytes.Say("Failed to stage application: insufficient resources"))
+
+			app := cf.Cf("app", appName)
+			Eventually(app).Should(Exit(1))
+			Ω(app.Out).Should(gbytes.Say("message: Insufficient resources"))
+		})
 	})
 })
